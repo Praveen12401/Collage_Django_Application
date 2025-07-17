5c0a2c701eaadfe2dc25fe6a5a972a5f6deb0973
Praveen <120632172+Praveen12401@users.noreply.github.com>
2025-07-17 15:15:00 +0000

    virtual envirment create and work

--- Collage_Application/Application/settings.py
+++ Collage_Application/Application/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'editor',
+    'django_clerk',
  
     
 ]
@@ -62,6 +63,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django_clerk.middleware.ClerkMiddleware',
 ]
 
 ROOT_URLCONF = "Application.urls"
