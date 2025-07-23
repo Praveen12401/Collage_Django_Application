e17e9a8451869095a6e6b43bcc3ce3cc227fbe5f
Praveen <120632172+Praveen12401@users.noreply.github.com>
2025-07-23 07:31:30 +0000

    ok

--- Collage_Application/editor/admin.py
+++ Collage_Application/editor/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UploadedVideo
+from .models import UploadedVideo,User
 
 # Register your models here.
 admin.site.register(UploadedVideo)
+admin.site.register(User)
--- Collage_Application/editor/views.py
+++ Collage_Application/editor/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render ,redirect
 from django.http import HttpResponse
+from .models import User
+from django.contrib import messages
+from django.contrib.auth.hashers import make_password
 
 # Create your views here.
 # import os
@@ -33,7 +36,26 @@ def contact_view(request):
 
 def user_form(request):   
     if request.method == 'POST':
-        pass
+        username=request.POST['username']
+        useremail=request.POST['email']
+        userpassword=request.POST['password']
+        print(username,useremail,userpassword)
+
+          # Simple check if user exists
+        if User.objects.filter(email=useremail).exists():
+            messages.error(request, "Email already registered")
+            return redirect("user_form")
+
+        # Save user with hashed password
+        user = User(
+            name=username, 
+            email=useremail,
+            password=make_password(userpassword)  # hash password
+        )
+        user.save()
+        messages.success(request, "User registered successfully!")
+        return redirect('/')  # or wherever you want
+        
     return  render(request,'editor/user_form.html')
 
     
